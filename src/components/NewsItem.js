d0c9c67ca77f5b8afbb18b328edf63ff63809014
pavan1815 <pavanreddyvancha@gmail.com>
2023-07-27 19:16:00 +0530

    1st

---
+++ src/components/NewsItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+    render() {
+        let {title,description,imageUrl,newsUrl,author,date,source} = this.props
+        return (
+            <div className="my-3">
+            <div className="card" style={{width: "18rem"}}>
+            <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%' , zIndex :'1'}} >{source}</span>
+                <img src={imageUrl?imageUrl:"https://png.pngtree.com/png-vector/20230107/ourmid/pngtree-new-update-banner-label-notification-template-png-image_6554704.png"} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">{title}...</h5>
+                        <p className="card-text">{description}...</p>
+                        <p className="card-text"> <small className="text-danger">By {author ? author : "Unknown"} on {new Date(date).toGMTString()} </small> </p>
+                        <a href={newsUrl} target='_blank' className="btn btn-sm btn-dark" rel="noreferrer">Read more</a>
+                    </div>
+            </div>
+            </div>
+        )
+    }
+}
+
+export default NewsItem
